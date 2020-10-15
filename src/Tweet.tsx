c8c1dd1a4c8141e85bc8a6e234e23fef356f1d71
AdamCsordas21 <adamcsordas@hotmail.com>
2020-10-15 19:23:07 +0100

    Add fontawesome icons

--- src/Tweet.tsx
+++ src/Tweet.tsx
@@ -1,31 +1,45 @@
-import React, { FC } from 'react';
-import styled from 'styled-components';
+import React, { FC } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCommentDots,
+  faRetweet,
+  faHeart,
+  faShare,
+} from "@fortawesome/free-solid-svg-icons";
 
 export interface Author {
-  name: string
-  tag: string
+  name: string;
+  tag: string;
 }
 
 export interface TweetProps {
-  author: Author
-  body: string
-  createdOn: string
+  author: Author;
+  body: string;
+  createdOn: string;
 }
 
 const TweetCreatedOn = styled.span`
   font-size: 0.8em;
 
   &::before {
-    content: '· ';
+    content: "· ";
   }
-`
+`;
 
 const Tweet: FC<TweetProps> = ({ author, body, createdOn }) => (
   <article data-testid="tweet">
-    <h3>{author.name} {author.tag} <TweetCreatedOn>{createdOn}</TweetCreatedOn></h3>
+    <h3>
+      {author.name} {author.tag} <TweetCreatedOn>{createdOn}</TweetCreatedOn>
+    </h3>
     {body}
-    <h5>reply retweet like share</h5>
+    <h5>
+      <FontAwesomeIcon icon={faCommentDots} />{" "}
+      <FontAwesomeIcon icon={faRetweet} />{" "}
+      <FontAwesomeIcon icon={faHeart} />{" "}
+      <FontAwesomeIcon icon={faShare} />
+    </h5>
   </article>
-)
+);
 
 export default Tweet;
