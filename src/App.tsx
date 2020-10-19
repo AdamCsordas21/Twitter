ee66d237c1821e1228e5e806cd29eccfd913c26f
AdamCsordas21 <adamcsordas@hotmail.com>
2020-10-19 16:39:27 +0100

    add twitter-clone icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from 'react';
 import './App.css';
 import Tweet, { TweetProps } from './Tweet';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDove
+} from "@fortawesome/free-solid-svg-icons";
 
 interface AppProps {
   tweets: TweetProps[]
@@ -8,7 +12,7 @@ interface AppProps {
 
 const App: FC<AppProps> = ({ tweets }) => (
   <div className="App">
-    <header>Home</header>
+    <header><FontAwesomeIcon icon={faDove} />{" "}Home</header>
     <main data-testid="main-section">
       {tweets.map((tweet, index) => <Tweet key={index} {...tweet} />)}
     </main>
