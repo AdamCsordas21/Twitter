86128c89a77cad9de378452e712def595d5a0071
AdamCsordas21 <adamcsordas@hotmail.com>
2020-10-21 19:05:34 +0100

    Add NewTweetButton

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,14 @@ const NewTweetInput = styled.textarea`
   border-radius: 2px;
 `;
 
+const NewTweetButton = styled.button`
+  height: 2.5em;
+  border-radius: 0.5em;
+  border: none;
+  color: white;
+  background-color: lightblue;
+`
+
 interface AppProps {
   tweets: TweetProps[];
 }
@@ -34,7 +42,10 @@ const App: FC<AppProps> = ({ tweets }) => (
       <div>
         <img src={userImg} alt="User Profile Img" />
       </div>
-      <NewTweetInput placeholder="What's happening?"></NewTweetInput>
+      <form>
+        <NewTweetInput placeholder="What's happening?" />
+        <NewTweetButton>Tweet</NewTweetButton>
+      </form>
       <h5>
         <FontAwesomeIcon icon={faImage} />{" "}
         <FontAwesomeIcon icon={faGrinSquintTears} />{" "}
