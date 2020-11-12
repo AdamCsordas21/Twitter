4ff200bb33dbac40e069d0fba147acf7ce19b7c2
AdamCsordas21 <adamcsordas@hotmail.com>
2020-11-12 18:39:18 +0000

    display x minutes ago if time diff is under 1h ago

--- src/date-time/date-time.spec.ts
+++ src/date-time/date-time.spec.ts
@@ -1,15 +1,21 @@
 import { formatDate } from "./date-time";
 
 describe("date formatter", () => {
-  it("should display full date if date is over 1 day ago", () => {
+  it("should display full date if date difference is over 1 day ago", () => {
     const date = new Date(2000, 0, 11, 5, 45, 55, 123).toJSON();
     const currentDate = new Date(2000, 0, 12, 17, 45, 55, 123);
     expect(formatDate(date, currentDate)).toEqual("11/01/2000");
   });
 
-  it("should display x hours ago if date is within 24 and 1 hour ago", () => {
+  it("should display x hours ago if time difference is within 24 and 1 hour ago", () => {
     const date = new Date(2000, 0, 31, 5, 45, 55, 123).toJSON();
     const currentDate = new Date(2000, 0, 31, 17, 45, 55, 123);
     expect(formatDate(date, currentDate)).toEqual("12h");
-  })
+  });
+
+  it("should display x minutes ago if time difference is under 1 hour ago", () => {
+    const date = new Date(2000, 0, 31, 16, 55, 55, 123).toJSON();
+    const currentDate = new Date(2000, 0, 31, 17, 50, 45, 123);
+    expect(formatDate(date, currentDate)).toEqual("55m");
+  });
 });
--- src/date-time/date-time.ts
+++ src/date-time/date-time.ts
@@ -3,15 +3,20 @@ const getLongDate = (date: Date): string => {
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
   return `${day}/${month}/${year}`;
-}
+};
 
-export const formatDate = (serialisedDate: string, currentDate: Date = new Date()): string => {
-  const date = new Date(serialisedDate)
-  const hour = 1000 * 60 * 60
-  const diff = (currentDate.getTime() - date.getTime()) / hour
-  if (diff > 24) {
-    return getLongDate(date)
-  } else {
-    return Math.ceil(diff) + "h"
+export const formatDate = (
+  serialisedDate: string,
+  currentDate: Date = new Date()
+): string => {
+  const date = new Date(serialisedDate);
+  const hour = 1000 * 60 * 60;
+  const diff = (currentDate.getTime() - date.getTime()) / hour;
+  if (diff < 1) {
+    return Math.ceil(diff * 60) + "m";
   }
-}
+  if (diff < 24) {
+    return Math.ceil(diff) + "h";
+  }
+  return getLongDate(date);
+};
