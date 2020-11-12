f04036461fc3f6d45b61c61347aeff8ef9fdb89e
AdamCsordas21 <adamcsordas@hotmail.com>
2020-11-12 19:19:57 +0000

    Style icons and add likes

--- src/Tweet.tsx
+++ src/Tweet.tsx
@@ -7,7 +7,7 @@ import {
   faHeart,
   faShare,
 } from "@fortawesome/free-solid-svg-icons";
-import { TweetModel } from './';
+import { TweetModel } from "./";
 import { formatDate } from "./date-time/date-time";
 
 const TweetCreatedOn = styled.span`
@@ -18,18 +18,36 @@ const TweetCreatedOn = styled.span`
   }
 `;
 
-const Tweet: FC<TweetModel> = ({ author, body, createdOn }) => (
+const FlexBox = styled.div`
+  padding-top: 0.5em;
+  display: flex;
+`;
+
+const FlexItemLabel = styled.label`
+  flex: 1 1 auto;
+`;
+
+const Tweet: FC<TweetModel> = ({ author, body, createdOn, likes }) => (
   <article className="tweet" data-testid="tweet">
     <h3>
-      {author.name} {author.tag} <TweetCreatedOn>{formatDate(createdOn)}</TweetCreatedOn>
+      {author.name} {author.tag}{" "}
+      <TweetCreatedOn>{formatDate(createdOn)}</TweetCreatedOn>
     </h3>
     {body}
-    <h5>
-      <FontAwesomeIcon icon={faCommentDots} />{" "}
-      <FontAwesomeIcon icon={faRetweet} />{" "}
-      <FontAwesomeIcon icon={faHeart} />{" "}
-      <FontAwesomeIcon icon={faShare} />
-    </h5>
+    <FlexBox>
+      <FlexItemLabel>
+        <FontAwesomeIcon icon={faCommentDots} />
+      </FlexItemLabel>
+      <FlexItemLabel>
+        <FontAwesomeIcon icon={faRetweet} />
+      </FlexItemLabel>
+      <FlexItemLabel>
+        <FontAwesomeIcon icon={faHeart} /> {likes}
+      </FlexItemLabel>
+      <FlexItemLabel>
+        <FontAwesomeIcon icon={faShare} />
+      </FlexItemLabel>
+    </FlexBox>
   </article>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,7 @@ export interface TweetModel {
   author: Author;
   body: string;
   createdOn: string;
+  likes?: number;
 }
 
 export interface Author {
