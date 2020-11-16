dea98efa4de085959b5f8476cf13a6b54c17ca18
AdamCsordas21 <adamcsordas@hotmail.com>
2020-11-16 20:02:58 +0000

    Add first test

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -1,13 +1,21 @@
 describe("home page", () => {
   const login = () => {
-    cy.visit("http://localhost:3000/")
-    cy.get("[placeholder=user]").clear().type("osh")
-    cy.get("[placeholder=password]").clear().type("secret2")
-    cy.contains("log in").click()
-  }
+    cy.visit("http://localhost:3000/");
+    cy.get("[placeholder=user]").clear().type("osh");
+    cy.get("[placeholder=password]").clear().type("secret2");
+    cy.contains("log in").click();
+  };
 
   it("lets user read tweets", () => {
-    login()
-    cy.get("[data-testid=tweet]").contains("Hello World")
-  })
-})
+    login();
+    cy.get("[data-testid=tweet]").contains("Hello World");
+  });
+
+  it("lets user create a tweet", () => {
+    login();
+    const tweetBody = "such tweet, very contents, wow";
+    cy.get('[placeholder="What\'s happening?"]').type(tweetBody);
+    cy.contains("Tweet").click();
+    cy.get("[data-testid=tweet]").within(() => cy.contains(tweetBody));
+  });
+});
