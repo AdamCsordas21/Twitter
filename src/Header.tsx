6d4db4a8208185c8463f74465134811784986631
AdamCsordas21 <adamcsordas@hotmail.com>
2020-11-23 21:39:54 +0000

    Add style to Home header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,12 @@
 import React, { FC } from "react";
+import styled from "styled-components";
 
-const Header: FC = () => <header>Home</header>;
+const Home = styled.header`
+  font-size: 1.2rem;
+  font-weight: 800;
+  line-height: 1.3125;
+`
+
+const Header: FC = () => <Home>Home</Home>;
 
 export default Header;
